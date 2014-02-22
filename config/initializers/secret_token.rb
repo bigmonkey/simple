9f432c38d7aff8e838fa402ffc2f2716475fb58c
BigMonkey <conway@xerpi.com>
2014-02-21 18:45:00 -0600

    heroku unicorn setting, database setting, application.yml setting

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Simple::Application.config.secret_key_base = '789542491e1295475e94f97804d0da7878066bcd67b6d542d3b77c816cca3537331daca58db72d4c022f3eeba98c88611a3bc472297e125cb879415fc6d43d28'
+Simple::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
